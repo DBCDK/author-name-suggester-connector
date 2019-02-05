88934782010e1a65a87e34152b16afedb7ca9e58
Jan Bauer Nielsen <jbn@dbc.dk>
2019-02-05 10:45:06 +0100

    Suggester returns ner-names as list-of-lists

--- src/main/java/dk/dbc/authornamesuggester/AuthorNameSuggestions.java
+++ src/main/java/dk/dbc/authornamesuggester/AuthorNameSuggestions.java
@@ -5,11 +5,16 @@
 
 package dk.dbc.authornamesuggester;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AuthorNameSuggestions {
 
@@ -21,7 +26,7 @@ public class AuthorNameSuggestions {
     @JsonProperty("ner-names")
     @JacksonXmlProperty(localName = "ner-name")
     @JacksonXmlElementWrapper(localName = "ner-names")
-    private List<String> nerNames;
+    private List<List<String>> nerNames;
 
     public List<String> getAuthorityIds() {
         return authorityIds;
@@ -31,12 +36,22 @@ public class AuthorNameSuggestions {
         this.authorityIds = authorityIds;
     }
 
+    @JsonIgnore
     public List<String> getNerNames() {
-        return nerNames;
+        if (this.nerNames != null && !this.nerNames.isEmpty()) {
+            return this.nerNames.stream()
+                    .flatMap(Collection::stream)
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
     }
 
+    @JsonIgnore
     public void setNerNames(List<String> nerNames) {
-        this.nerNames = nerNames;
+        if (nerNames != null) {
+            this.nerNames = new ArrayList<>();
+            this.nerNames.add(nerNames);
+        }
     }
 
     @Override
